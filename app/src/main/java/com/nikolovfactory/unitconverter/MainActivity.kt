066558d77ccfc32802986921c7bc580d7aabe86c
atanas-nikolov <master89q@gmail.com>
2024-09-17 17:58:09 +0300

    add Two Text Boxes and select option for them

--- app/src/main/java/com/nikolovfactory/unitconverter/MainActivity.kt
+++ app/src/main/java/com/nikolovfactory/unitconverter/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.nikolovfactory.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.nikolovfactory.unitconverter.ui.theme.UnitConverterTheme
 
@@ -42,8 +49,18 @@ fun UnitConverter () {
         OutlinedTextField(value = "", onValueChange = {})
 
         Row {
-            // Here all the UI elements will be stacked next to each other
-
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                   Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+            }
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+            }
 
         }
         Text(text = "Result:")
